57ec4cf185653cb396bf21f281a2963a25b9b708
Marcel Corso <marcel.corso@gmail.com>
2014-11-17 12:22:54 +0100

    a graph

--- graph.go
+++ graph.go
@@ -1,17 +1,68 @@
 package nuruu
 
-type Vertex struct {
-  key int
-  edges LinkedList
+import (
+  "fmt"
+)
+
+type Edge struct {
+  row, column string
+  // a sorted linked list happens here. it could be a map too. nah. later maybe.
+  Next *Edge
 }
 
 type Graph struct {
-	vertices LinkedList
+	rows map[string]*Edge
 }
 
+func NewGraph() *Graph {
+    g := new(Graph)
+    g.rows = make(map[string]*Edge)
+    return g
+}
 
-func (ll *Graph) addEdge(a int, b int) {
-  // find a and b in the vertices, create if not there
-  
+func (g *Graph) Add(row string, column string) bool {
+  if row == column {
+    return false
+  }
+  // be sure row < column
+  if row < column {
+    row, column = column, row
+  }
+
+  edge := Edge{row, column, nil}
+
+  var previous *Edge = nil
+  p := g.rows[row]
+
+  for ;; {
+    if p == nil || p.column > column {
+      if previous == nil {
+        g.rows[row] = &edge
+        return true
+      } else {
+        previous.Next = &edge
+        return true
+      }
+    }
+    previous = p
+    p = p.Next
+  }
+
+}
+
+func (g *Graph) Adjacent(row string, column string) bool {
+  if row == column {
+    return false
+  }
+  // be sure row < column
+  if row < column {
+    row, column = column, row
+  }
+  for p := g.rows[row]; (p != nil && p.column <= column); p = p.Next {
+    if p.column == column {
+      return true
+    }
+  }
+  return false
 }
 
--- graph_test.go
+++ graph_test.go
@@ -3,4 +3,36 @@ package nuruu
 import "testing"
 
 func TestFirstTreeAdd(t *testing.T) {
+  g := NewGraph()
+  g.Add("a", "b")
+  g.Add("a", "c")
+  g.Add("b", "c")
+  g.Add("b", "d")
+  g.Add("c", "d")
+
+  if g.Adjacent("a", "b") == false {
+    t.Errorf("should have %d-%d edge", "a", "b")
+  }
+
+  if g.Adjacent("b", "a") == false {
+    t.Errorf("should have %d-%d edge", "b", "a")
+  }
+
+  if g.Adjacent("b", "d") == false {
+    t.Errorf("should have %d-%d edge", "b", "d")
+  }
+
+  if g.Adjacent("z", "a") == true {
+    t.Errorf("should NOT have %d-%d edge", "z", "a")
+  }
+
+  if g.Adjacent("a", "d") == true {
+    t.Errorf("should NOT have %d-%d edge", "c", "a")
+  }
+
+
+  if g.Adjacent("a", "a") == true {
+    t.Errorf("a-a edges are not allowed")
+  }
+
 }
